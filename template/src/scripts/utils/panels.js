d82f1b20e4505eddc1148b8814851f50971786d5
Piotr Kulpinski <piotr@kulpinski.pl>
2019-08-02 10:09:35 +0200

    Update panel scripts

--- template/src/scripts/utils/panels.js
+++ template/src/scripts/utils/panels.js
@@ -1,35 +1,48 @@
 const Panels = {
-  event: new CustomEvent('panels:close'),
-
   init() {
     window.closePanels = () => {
-      const bodyClasses = ['has-nav-open', 'has-menu-open'];
+      const bodyClasses = ['has-menu-open', 'has-popup-open'];
 
       document.body.classList.remove(...bodyClasses);
-      document.dispatchEvent(this.event);
+      document.dispatchEvent(new CustomEvent('panels:close'));
     };
 
-    const listener = ev => {
-      const name = 'dropdown';
-      const pattern = new RegExp(`${name}($|\s)`);
+    document.addEventListener('keyup', this.keyListener.bind(this), false);
+    document.addEventListener('click', this.clickListener.bind(this), false);
+    document.addEventListener('touchend', this.clickListener.bind(this), false);
+  },
 
-      let matched = ev.path.some(element => {
-        return pattern.test(element.className) || (element.dataset && element.dataset.toggle);
-      });
+  keyListener(ev) {
+    if (ev.keyCode === 27) {
+      window.closePanels();
+    }
+  },
 
-      if (!matched) {
-        window.closePanels();
+  clickListener(ev) {
+    const target = [ev.target];
+    const path = ev.path || (ev.composedPath && ev.composedPath());
+
+    if (path) {
+      if (!this.listenerMatcher(path, 'dropdown')) {
+        document.dispatchEvent(new CustomEvent('dropdown:close'));
       }
-    };
+    }
 
-    document.addEventListener('keyup', ev => {
-      if (ev.keyCode === 27) {
-        window.closePanels();
+    if (target) {
+      if (this.listenerMatcher(target, /popup($|\s)/)) {
+        document.dispatchEvent(new CustomEvent('popup:close'));
       }
+    }
+  },
+
+  listenerMatcher(path, keyword) {
+    return path.some(element => {
+      let isRegExp = keyword instanceof RegExp;
+      let matchClass = (element instanceof HTMLElement && element.className && (isRegExp ? keyword.test(element.className) : element.className.includes(keyword)));
+      let matchDataset = (element.dataset && element.dataset[keyword]);
+
+      return isRegExp ? matchClass : matchClass || matchDataset;
     });
-
-    document.addEventListener('click', listener);
-    document.addEventListener('touchend', listener);
   },
 };
 
